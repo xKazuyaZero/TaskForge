cf4a7662e5e73e4f73d5bdfab6aaf44cbb1b3443
xKazuya <cannizzaro1711@gmail.com>
2026-03-12 03:13:58 +0100

    Add storage interface for CLI dependencies

---
+++ TaskForge/TaskForge.Core/Storage/ITaskStorage.cs
@@ -0,0 +1,9 @@
+﻿using TaskForge.Core.Models;
+
+namespace TaskForge.Core.Storage;
+
+public interface ITaskStorage
+{
+    List<TaskItem> LoadTasks();
+    bool TrySaveTasks(IReadOnlyList<TaskItem> tasks, out string errorMessage);
+}
